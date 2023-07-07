0b9a913b6205d3e9bd524e7bab8a75b948377dba
Julián Agudelo <agudelojulian649@gmail.com>
2023-07-06 23:52:16 -0500

    Se realiza la segunda parte del proyecto integrador

--- Laberinto.py
+++ Laberinto.py
@@ -1,2 +1,17 @@
+from readchar import readkey, key
+"Parte I"
+
 nombre_jugador = input("Ingresar el nombre del jugador: ")
-print("Bienvenido al juego", nombre_jugador)
+print("Bienvenido al juego,", nombre_jugador)
+
+"""
+Parte II
+Bucle infinito, detecta que tecla se pulsa y se cierra con la flecha arriba
+"""
+
+while True:
+  k = readkey()
+  if k == key.UP:
+    break
+  else:
+    print(k)
