2d97991abac11d9b798d7e056b50aab9f62c5543
Julián Agudelo <agudelojulian649@gmail.com>
2023-07-16 21:17:57 -0500

    Se avanza con la parte iv del proyecto integrador

--- Laberinto.py
+++ Laberinto.py
@@ -11,11 +11,11 @@ Bucle infinito, detecta que tecla se pulsa y se cierra con la flecha arriba
 """
 
 while True:
-  k = readkey()
-  if k == key.UP:
-    break
-  else:
-    print(k)
+    k = readkey()
+    if k == key.UP:
+        break
+    else:
+        print(k)
 
 """
 Parte III
@@ -25,10 +25,84 @@ Manipulación de la consola mediante la libreria os
 def limpiar_terminal(a):
     num = 0
     while True:
-      k = readkey()
-      if k == 'n':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        num += 1
-        print(num)
-      if num == 50:
-        break
+        k = readkey()
+        if k == 'n':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            num += 1
+            print(num)
+        if num == 50:
+            break
+
+"""
+Parte IV
+Creación del mapa del laberinto
+"""
+
+mapa = """..###################
+..#.....#.......#...#
+#.#.#.#.###.#######.#
+#.#.#.#...........#.#
+#.#.#######.#######.#
+#.#...#...#.....#.#.#
+#.#.###.#.#.#####.#.#
+#...#...#.......#...#
+###.###.###.#.###.###
+#...#...#...#.......#
+#.#.###.#####.###.#.#
+#.#.#...#...#.#.#.#.#
+#####.#.#.#####.#.###
+#.....#...#...#.....#
+#.#.#####.#.###.###.#
+#.#...#...#.#.....#.#
+#.###.#.###.###.#.###
+#.#.#.#.#.......#...#
+#.#.#.#.###.###.#.###
+#...#.#.#...#...#...#
+###################.#
+"""
+
+def matriz(mapa: str):
+    matriz = []
+    for linea in mapa.split("\n"):
+        matriz.append(list(linea))
+    punto_final = (len(matriz)-1, len(matriz[0])-2)
+    return matriz, punto_final
+
+def pantalla(laberinto):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for i in laberinto:
+        print(i, sep='\n')
+
+def main_loop(mapa):
+    laberinto, coor_final = matriz(mapa)
+    py, px = 0, 0
+    personaje_coor = [py, px]
+    laberinto[py][px] = 'P'
+    while True:
+        k = readkey()
+        if k == key.UP:
+            py -= 1
+            if py != -1:
+                if laberinto[py][px] == '.':
+                    laberinto[py][px] = 'P'
+                    personaje_coor[0] += py
+                    pantalla(laberinto)
+                elif laberinto[py][px] == '#':
+                    py += 1
+                    pantalla(laberinto)
+            else:
+                py += 1
+                pantalla(laberinto)
+        if k == key.DOWN:
+            py += 1
+            if laberinto[py][px] == '.':
+                laberinto[py][px] = 'P'
+                personaje_coor[0] += py
+                pantalla(laberinto)
+            elif laberinto[py][px] == '#':
+                pantalla(laberinto)
+                py -= 1
+        if k == key.LEFT:
+            px -= 1
+            if px != -1:
+              #Continuar codigo
